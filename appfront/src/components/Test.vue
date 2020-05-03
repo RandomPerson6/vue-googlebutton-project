7f9a09c27e4643ca78995874d08e643dbb8114e7
Rosemary <rosemary@localhost.localdomain>
2020-05-03 10:49:15 -0400

    made google button, have not completed the customization

---
+++ appfront/src/components/Test.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="">
+    <div id="my-signin2"></div>
+    <div id="gSignInWrapper">
+      <div id="costomBtn" class="customGPlusSignIn">
+      <!-- Ref(haven't finished): https://developers.google.com/identity/sign-in/web/build-button -->
+        <span class="icon"></span>
+        <span class="buttonText">Google</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script src="https://apis.google.com/js/platform.js?onload=onLoadCallback" async defer></script>
+<script>
+export default {
+  name: 'ConstantTop',
+  mounted() {
+    gapi.signin2.render('my-signin2',{
+      onsuccess : this.onSuccess
+    });
+  },
+  methods: {
+    onSuccess: function(googleUser) {
+      var profile = googleUser.getBasicProfile();
+      console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+      console.log('Name: ' + profile.getName());
+      console.log('Image URL: ' + profile.getImageUrl());
+      console.log('Email: ' + profile.getEmail());
+    }
+  },
+  props: {
+  },
+  data: function() {
+    return {
+    };
+  },
+}
+</script>
+
+<style scoped>
+  .constanttop {
+    background: pink;
+    color: white;
+    width: 100%;
+  }
+  li {
+    list-style-type: none;
+    display: block;
+  }
+  img {
+    height:100px;
+  }
+</style>
